5376263fcd5a8d3e335bc1fd13d635c666e9cf8f
jp <josepaulo1607@gmail.com>
2024-12-13 13:26:38 -0300

    exibindo o footer

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
+//cotacao da moeda
+const USD = 4.87
+const EUR = 5.13
+const GBP = 4.38
+
+//pegando o formulario
 const form = document.querySelector("form");
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 // quando executar alguma acao, o addEventListener vai monitorar quando entrar conteudo no input 
 //e logo em seguida vai executar uma funcao
@@ -17,5 +24,37 @@ form.onsubmit = function(){
     // para nao fazer o reload da pagina qnd enviar o submit
     event.preventDefault();
 
-    console.log(currency.value)
+    switch(currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, "US$");
+            //para nao olhar para outros casos
+            break;
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€");
+            break;
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£");
+            break;
+        default:
+            alert("Selecione uma moeda válida");
+            break;  // caso nao seja nenhuma das opcoes, exibe uma mensagem de alerta 
+                         // e para o script não executar a funcao convertCurrency 
+                         // (caso queira adicionar mais moedas, basta adicionar mais cases nesse switch) 
+                         // e deixar o default break;  // sem ele, caso nao seja nenhuma das opcoes, o script vai continuar executando a funcao convertCurrency
+    }
+
 }
+//estrutura da conversao e funcao para conversao
+function convertCurrency(amount, price, symbol) {
+    try {
+        //aplica a classe para exibir o footer com resultado 
+        footer.classList.add("show-result")
+    } catch (error) {
+        //oculta a classe do footer
+        footer.classList.remove("show-result")
+        
+        console.log(error);
+        alert("Ocorreu um erro durante a conversão. Tente novamente mais tarde.");
+    }
+}
+    
