00b6e2cc9219ce4ca73f4bd12831f4f0fafcce14
jp <josepaulo1607@gmail.com>
2024-12-13 08:30:22 -0300

    validando o input

--- scripts.js
+++ scripts.js
@@ -5,5 +5,8 @@ const amount = document.getElementById("amount")
 
 //minupulando amount para receber somente numeros
 amount.addEventListener("input",()=>{
-   console.log(amount.value)
+    // verifica caracteres se tiver apenas numeros e vai subistituir letras por nada 
+    const HasCharacterRegex = /\D+/g
+    // replace é utilizado para trocar para nada  
+    amount.value= amount.value.replace(HasCharacterRegex, "")
 }) 
